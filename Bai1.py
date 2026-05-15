d0b665f3b73e1eb81b859694c09a648e8dc767e3
thanhvu134 <vu_2151220285@dau.edu.vn>
2026-05-15 11:07:13 +0700

    Delete Bai1.py

--- Bai1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        seen = {}
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in seen:
-                return [seen[diff], i]
-            seen[num] = i
